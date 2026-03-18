229bc93ac7e20e2b7c073b5fdda3c94ef8f7cca5
Aman0039 <pathakaman0532gmail.com>
2026-03-19 03:52:42 +0530

    Project Setup

---
+++ client/src/component/Navbar.tsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="logo">Hire Hope</div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
